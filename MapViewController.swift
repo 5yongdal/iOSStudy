79d2095546867b5c4ea2a12848f4f1cb6db2ef78
박성진 <parksj@nhn.com>
2017-06-01 15:46:15 +0900

    6장 예제 완료

--- MapViewController.swift
+++ MapViewController.swift
@@ -7,8 +7,55 @@
 //
 
 import UIKit
+import MapKit
 
 class MapViewController: UIViewController {
     
+    var mapView: MKMapView!
+    
+    override func loadView() {
+        // 지도 뷰 생성
+        mapView = MKMapView()
+        
+        // 지도 뷰를 뷰 컨트롤러의 view로 설정
+        view = mapView
+        
+        let segmentedControl = UISegmentedControl(items: ["Standard", "Hybrid", "Satellite"])
+        segmentedControl.backgroundColor = UIColor.white.withAlphaComponent(0.5)
+        segmentedControl.selectedSegmentIndex = 0
+        
+        segmentedControl.addTarget(self, action: Selector(("mapTypeChanged")), for: .valueChanged)
+        
+        segmentedControl.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(segmentedControl)
+        
+        let topConstraint = segmentedControl.topAnchor.constraint(equalTo: topLayoutGuide.bottomAnchor, constant: 8)
+        
+        let margins = view.layoutMarginsGuide
+        
+        let leadingConstraint = segmentedControl.leadingAnchor.constraint(equalTo: margins.leadingAnchor)
+        let trailingContraint = segmentedControl.trailingAnchor.constraint(equalTo: margins.trailingAnchor)
+        
+        topConstraint.isActive = true
+        leadingConstraint.isActive = true
+        trailingContraint.isActive = true
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    func mapTypeChanged(segControl: UISegmentedControl) {
+        switch segControl.selectedSegmentIndex {
+        case 0 :
+            mapView.mapType = .standard
+        case 1 :
+            mapView.mapType = .hybrid
+        case 2 :
+            mapView.mapType = .satellite
+        default:
+            break
+        }
+    }
 }
-정
